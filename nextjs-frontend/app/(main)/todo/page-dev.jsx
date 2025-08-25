9e86318506eb972beb6157f78db91996c60234a1
Amit Yadav <22bcs030@iiitdmj.ac.in>
2025-08-26 01:45:55 +0530

    Frontend: add feature-specific page templates and layouts

---
+++ nextjs-frontend/app/(main)/todo/page-dev.jsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { useEffect } from "react";
+import SimpleTodoPage from "@/components/SimpleTodoPage";
+
+export default function TodoPage() {
+  return <SimpleTodoPage />;
+}
